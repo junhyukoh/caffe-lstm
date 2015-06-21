6b079b5124f3ff2ae7de618e4ac8b72b96af7c6e
junhyuk <junhyuk@umich.edu>
2015-06-21 17:17:06 -0400

    fixed a bug in lstm mini-batch

--- src/caffe/layers/lstm_layer.cpp
+++ src/caffe/layers/lstm_layer.cpp
@@ -29,6 +29,7 @@ void LstmLayer<Dtype>::LayerSetUp(const vector<Blob<Dtype>*>& bottom,
         this->layer_param_.lstm_param().weight_filler()));
  
     // input-to-hidden weights
+    // Intialize the weight
     vector<int> weight_shape;
     weight_shape.push_back(4*H_);
     weight_shape.push_back(I_);
@@ -36,13 +37,14 @@ void LstmLayer<Dtype>::LayerSetUp(const vector<Blob<Dtype>*>& bottom,
     weight_filler->Fill(this->blobs_[0].get());
 
     // hidden-to-hidden weights
+    // Intialize the weight
     weight_shape.clear();
     weight_shape.push_back(4*H_);
     weight_shape.push_back(H_);
     this->blobs_[1].reset(new Blob<Dtype>(weight_shape));
     weight_filler->Fill(this->blobs_[1].get());
 
-    // bias
+    // If necessary, intiialize and fill the bias term
     vector<int> bias_shape(1, 4*H_);
     this->blobs_[2].reset(new Blob<Dtype>(bias_shape));
     shared_ptr<Filler<Dtype> > bias_filler(GetFiller<Dtype>(
@@ -85,8 +87,8 @@ void LstmLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom,
   // Gate initialization
   vector<int> gate_shape;
   gate_shape.push_back(T_);
-  gate_shape.push_back(4);
   gate_shape.push_back(N_);
+  gate_shape.push_back(4);
   gate_shape.push_back(H_);
   pre_gate_.Reshape(gate_shape);
   gate_.Reshape(gate_shape);
@@ -98,8 +100,6 @@ void LstmLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom,
   cell_.Reshape(top_shape);
   tanh_cell_.Reshape(top_shape);
   clip_mask_.Reshape(top_shape);
-
-  // Use a shared Blob with different shape for convinience
   top_.Reshape(top_shape);
   top_.ShareData(*top[0]);
   top_.ShareDiff(*top[0]);
@@ -152,48 +152,60 @@ void LstmLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
   // Add bias 
   caffe_cpu_gemm(CblasNoTrans, CblasNoTrans, T_*N_, 4*H_, 1, (Dtype)1.,
       bias_multiplier_.cpu_data(), bias, (Dtype)1., pre_gate_data);
-  
+
   // Compute recurrent forward propagation
   for (int t = 0; t < T_; ++t) {
     Dtype* h_t = top_data + top_.offset(t);
     Dtype* c_t = cell_data + cell_.offset(t);
     Dtype* tanh_c_t = tanh_cell_data + tanh_cell_.offset(t);
     Dtype* i_t = gate_data + gate_.offset(t);
-    Dtype* f_t = gate_data + gate_.offset(t, 1); 
-    Dtype* o_t = gate_data + gate_.offset(t, 2); 
-    Dtype* g_t = gate_data + gate_.offset(t, 3); 
+    Dtype* f_t = gate_data + gate_.offset(t, 0, 1); 
+    Dtype* o_t = gate_data + gate_.offset(t, 0, 2); 
+    Dtype* g_t = gate_data + gate_.offset(t, 0, 3); 
     Dtype* pre_i_t = pre_gate_data + pre_gate_.offset(t);
-    Dtype* pre_g_t = pre_gate_data + pre_gate_.offset(t, 3);
+    Dtype* pre_g_t = pre_gate_data + pre_gate_.offset(t, 0, 3);
     Dtype* mask_t = mask + clip_mask_.offset(t);
+    const Dtype* h_t_1 = t > 0 ? (h_t - top_.offset(1)) : h_0_.cpu_data();
+    const Dtype* c_t_1 = t > 0 ? (c_t - cell_.offset(1)) : c_0_.cpu_data();
 
     // Hidden-to-hidden propagation
-    const Dtype* h_t_1 = t > 0 ? (h_t - top_.offset(1)) : h_0_.cpu_data();
     if (clip) {
       caffe_mul(N_*H_, h_t_1, mask_t, clipped_.mutable_cpu_data());
       h_t_1 = clipped_.cpu_data();
     }
     caffe_cpu_gemm(CblasNoTrans, CblasTrans, N_, 4*H_, H_, (Dtype)1.,
-        h_t_1, weight_h, (Dtype)1., pre_i_t);
+        h_t_1, weight_h, (Dtype)1., pre_gate_data + pre_gate_.offset(t));
 
-    // Apply nonlinearity
-    caffe_sigmoid(3*N_*H_, pre_i_t, i_t);
-    caffe_tanh(N_*H_, pre_g_t, g_t);
+    for (int n = 0; n < N_; ++n) {
+      // Apply nonlinearity
+      caffe_sigmoid(3*H_, pre_i_t, i_t);
+      caffe_tanh(H_, pre_g_t, g_t);
+      if (clip && mask_t[0] == 0) {
+        caffe_set(H_, (Dtype)0., f_t);
+      }
 
-    // Compute cell : c(t) = f(t)*c(t-1) + i(t)*g(t)
-    const Dtype* c_t_1 = t > 0 ? (c_t - cell_.offset(1)) : c_0_.cpu_data();
-    if (clip) {
-      caffe_mul(N_*H_, c_t_1, mask_t, clipped_.mutable_cpu_data());
-      c_t_1 = clipped_.cpu_data();
+      // Compute cell : c(t) = f(t)*c(t-1) + i(t)*g(t)
+      caffe_mul(H_, f_t, c_t_1, c_t);
+      caffe_mul(H_, i_t, g_t, ig);
+      caffe_add(H_, c_t, ig, c_t);
+
+      // Compute output 
+      caffe_tanh(H_, c_t, tanh_c_t);
+      caffe_mul(H_, o_t, tanh_c_t, h_t);
+      
+      h_t += H_;
+      c_t += H_;
+      tanh_c_t += H_;
+      c_t_1 += H_;
+      i_t += 4*H_;
+      f_t += 4*H_;
+      o_t += 4*H_;
+      g_t += 4*H_;
+      pre_i_t += 4*H_;
+      pre_g_t += 4*H_;
+      mask_t += H_;
     }
-    caffe_mul(N_*H_, f_t, c_t_1, c_t);
-    caffe_mul(N_*H_, i_t, g_t, ig);
-    caffe_add(N_*H_, c_t, ig, c_t);
-
-    // Compute output 
-    caffe_tanh(N_*H_, c_t, tanh_c_t);
-    caffe_mul(N_*H_, o_t, tanh_c_t, h_t);
   }
-
   // Preserve cell state and output value for truncated BPTT
   caffe_copy(N_*H_, cell_data + cell_.offset(T_-1), c_T_.mutable_cpu_data());
   caffe_copy(N_*H_, top_data + top_.offset(T_-1), h_T_.mutable_cpu_data());
@@ -228,62 +240,80 @@ void LstmLayer<Dtype>::Backward_cpu(const vector<Blob<Dtype>*>& top,
     Dtype* dc_t = cell_diff + cell_.offset(t);
     const Dtype* tanh_c_t = tanh_cell_data + tanh_cell_.offset(t); 
     const Dtype* i_t = gate_data + gate_.offset(t);
-    const Dtype* f_t = gate_data + gate_.offset(t, 1); 
-    const Dtype* o_t = gate_data + gate_.offset(t, 2); 
-    const Dtype* g_t = gate_data + gate_.offset(t, 3); 
+    const Dtype* f_t = gate_data + gate_.offset(t, 0, 1); 
+    const Dtype* o_t = gate_data + gate_.offset(t, 0, 2); 
+    const Dtype* g_t = gate_data + gate_.offset(t, 0, 3); 
     Dtype* di_t = gate_diff + gate_.offset(t);
-    Dtype* df_t = gate_diff + gate_.offset(t, 1); 
-    Dtype* do_t = gate_diff + gate_.offset(t, 2); 
-    Dtype* dg_t = gate_diff + gate_.offset(t, 3); 
+    Dtype* df_t = gate_diff + gate_.offset(t, 0, 1); 
+    Dtype* do_t = gate_diff + gate_.offset(t, 0, 2); 
+    Dtype* dg_t = gate_diff + gate_.offset(t, 0, 3); 
     Dtype* pre_di_t = pre_gate_diff + pre_gate_.offset(t);
-    Dtype* pre_dg_t = pre_gate_diff + pre_gate_.offset(t, 3);
+    Dtype* pre_dg_t = pre_gate_diff + pre_gate_.offset(t, 0, 3);
     Dtype* fdc = fdc_.mutable_cpu_data();
     const Dtype* mask_t = mask + clip_mask_.offset(t);
 
-    // Output gate : tanh(c(t)) * h_diff(t)
-    caffe_mul(N_*H_, tanh_c_t, dh_t, do_t);
+    for (int n = 0; n < N_; ++n) {
+      // Output gate : tanh(c(t)) * h_diff(t)
+      caffe_mul(H_, tanh_c_t, dh_t, do_t);
 
-    // Cell state : o(t) * tanh'(c(t)) * h_diff(t) + f(t+1) * c_diff(t+1)
-    caffe_mul(N_*H_, o_t, dh_t, dc_t);
-    caffe_tanh_diff(N_*H_, tanh_c_t, dc_t, dc_t);
-    if (t < T_-1) {
-      caffe_mul(N_*H_, f_t + gate_.offset(1), dc_t + cell_.offset(1), fdc);
-      if (clip) {
-        caffe_mul(N_*H_, mask_t + clip_mask_.offset(1), fdc, fdc);
+      // Cell state : o(t) * tanh'(c(t)) * h_diff(t) + f(t+1) * c_diff(t+1)
+      caffe_mul(H_, o_t, dh_t, dc_t);
+      caffe_tanh_diff(H_, tanh_c_t, dc_t, dc_t);
+      if (t < T_-1) {
+        caffe_mul(H_, f_t + gate_.offset(1), dc_t + cell_.offset(1), fdc);
+        caffe_add(H_, fdc, dc_t, dc_t);
       }
-      caffe_add(N_*H_, fdc, dc_t, dc_t);
-    }
 
-    // Forget gate : c(t-1) * c_diff(t)
-    const Dtype* c_t_1 = t > 0 ? (c_t - cell_.offset(1)) : c_0_.cpu_data();
-    if (clip) {
-      caffe_mul(N_*H_, c_t_1, mask_t, clipped_.mutable_cpu_data());
-      c_t_1 = clipped_.cpu_data();
-    }
-    caffe_mul(N_*H_, c_t_1, dc_t, df_t);
+      // Forget gate : c(t-1) * c_diff(t)
+      const Dtype* c_t_1 = t > 0 ? (c_t - cell_.offset(1)) : c_0_.cpu_data();
+      if (clip) {
+        caffe_mul(H_, c_t_1, mask_t, clipped_.mutable_cpu_data());
+        c_t_1 = clipped_.cpu_data();
+      }
+      caffe_mul(H_, c_t_1, dc_t, df_t);
 
-    // Input gate : g(t) * c_diff(t)
-    caffe_mul(N_*H_, g_t, dc_t, di_t);
-    // Input modulation gate : i(t) * c_diff(t)
-    caffe_mul(N_*H_, i_t, dc_t, dg_t);
+      // Input gate : g(t) * c_diff(t)
+      caffe_mul(H_, g_t, dc_t, di_t);
+      // Input modulation gate : i(t) * c_diff(t)
+      caffe_mul(H_, i_t, dc_t, dg_t);
 
-    // Compute derivate before nonlinearity
-    caffe_sigmoid_diff(3*N_*H_, i_t, di_t, pre_di_t);
-    caffe_tanh_diff(N_*H_, g_t, dg_t, pre_dg_t);
+      // Compute derivate before nonlinearity
+      caffe_sigmoid_diff(3*H_, i_t, di_t, pre_di_t);
+      caffe_tanh_diff(H_, g_t, dg_t, pre_dg_t);
+      
+      // Clip deriviates before nonlinearity
+      if (clipping_threshold_ > 0.0f) {
+        caffe_bound(4*H_, pre_di_t, -clipping_threshold_, 
+            clipping_threshold_, pre_di_t);
+      }
 
-    // Clip deriviates before nonlinearity
-    if (clipping_threshold_ > 0.0f) {
-      caffe_bound(4*N_*H_, pre_di_t, -clipping_threshold_, 
-          clipping_threshold_, pre_di_t);
+      dh_t += H_;
+      c_t += H_;
+      dc_t += H_;
+      tanh_c_t += H_;
+      mask_t += H_;
+      di_t += 4*H_;
+      df_t += 4*H_;
+      do_t += 4*H_;
+      dg_t += 4*H_;
+      i_t += 4*H_;
+      f_t += 4*H_;
+      o_t += 4*H_;
+      g_t += 4*H_;
+      pre_di_t += 4*H_;
+      pre_dg_t += 4*H_;
     }
     
     if (t > 0) {
-      Dtype* dh_t_1 = dh_t - top_.offset(1);
+      Dtype* dh_t_1 = top_diff + top_.offset(t-1);
       // Backprop output errors to the previous time step
       caffe_cpu_gemm(CblasNoTrans, CblasNoTrans, N_, H_, 4*H_,
-        (Dtype)1., pre_di_t, weight_h, (Dtype)0., clipped_.mutable_cpu_data());
+          (Dtype)1., pre_gate_diff + pre_gate_.offset(t), 
+          weight_h, (Dtype)0., clipped_.mutable_cpu_data());
       if (clip) {
-        caffe_mul(N_*H_, clipped_.cpu_data(), mask_t, clipped_.mutable_cpu_data());
+        caffe_mul(N_*H_, clipped_.cpu_data(),
+          mask + clip_mask_.offset(t), 
+          clipped_.mutable_cpu_data());
       }
       caffe_add(N_*H_, dh_t_1, clipped_.cpu_data(), dh_t_1);
     }
--- src/caffe/layers/lstm_layer.cu
+++ src/caffe/layers/lstm_layer.cu
@@ -50,48 +50,60 @@ void LstmLayer<Dtype>::Forward_gpu(const vector<Blob<Dtype>*>& bottom,
   // Add bias 
   caffe_gpu_gemm(CblasNoTrans, CblasNoTrans, T_*N_, 4*H_, 1, (Dtype)1.,
       bias_multiplier_.gpu_data(), bias, (Dtype)1., pre_gate_data);
-  
+
   // Compute recurrent forward propagation
   for (int t = 0; t < T_; ++t) {
     Dtype* h_t = top_data + top_.offset(t);
     Dtype* c_t = cell_data + cell_.offset(t);
     Dtype* tanh_c_t = tanh_cell_data + tanh_cell_.offset(t);
     Dtype* i_t = gate_data + gate_.offset(t);
-    Dtype* f_t = gate_data + gate_.offset(t, 1); 
-    Dtype* o_t = gate_data + gate_.offset(t, 2); 
-    Dtype* g_t = gate_data + gate_.offset(t, 3); 
+    Dtype* f_t = gate_data + gate_.offset(t, 0, 1); 
+    Dtype* o_t = gate_data + gate_.offset(t, 0, 2); 
+    Dtype* g_t = gate_data + gate_.offset(t, 0, 3); 
     Dtype* pre_i_t = pre_gate_data + pre_gate_.offset(t);
-    Dtype* pre_g_t = pre_gate_data + pre_gate_.offset(t, 3);
+    Dtype* pre_g_t = pre_gate_data + pre_gate_.offset(t, 0, 3);
     Dtype* mask_t = mask + clip_mask_.offset(t);
+    const Dtype* h_t_1 = t > 0 ? (h_t - top_.offset(1)) : h_0_.gpu_data();
+    const Dtype* c_t_1 = t > 0 ? (c_t - cell_.offset(1)) : c_0_.gpu_data();
 
     // Hidden-to-hidden propagation
-    const Dtype* h_t_1 = t > 0 ? (h_t - top_.offset(1)) : h_0_.gpu_data();
     if (clip) {
       caffe_gpu_mul(N_*H_, h_t_1, mask_t, clipped_.mutable_gpu_data());
       h_t_1 = clipped_.gpu_data();
     }
     caffe_gpu_gemm(CblasNoTrans, CblasTrans, N_, 4*H_, H_, (Dtype)1.,
-        h_t_1, weight_h, (Dtype)1., pre_i_t);
+        h_t_1, weight_h, (Dtype)1., pre_gate_data + pre_gate_.offset(t));
 
-    // Apply nonlinearity
-    caffe_gpu_sigmoid(3*N_*H_, pre_i_t, i_t);
-    caffe_gpu_tanh(N_*H_, pre_g_t, g_t);
+    for (int n = 0; n < N_; ++n) {
+      // Apply nonlinearity
+      caffe_gpu_sigmoid(3*H_, pre_i_t, i_t);
+      caffe_gpu_tanh(H_, pre_g_t, g_t);
+      if (clip) {
+        caffe_gpu_mul(H_, f_t, mask_t, f_t);
+      }
 
-    // Compute cell : c(t) = f(t)*c(t-1) + i(t)*g(t)
-    const Dtype* c_t_1 = t > 0 ? (c_t - cell_.offset(1)) : c_0_.gpu_data();
-    if (clip) {
-      caffe_gpu_mul(N_*H_, c_t_1, mask_t, clipped_.mutable_gpu_data());
-      c_t_1 = clipped_.gpu_data();
+      // Compute cell : c(t) = f(t)*c(t-1) + i(t)*g(t)
+      caffe_gpu_mul(H_, f_t, c_t_1, c_t);
+      caffe_gpu_mul(H_, i_t, g_t, ig);
+      caffe_gpu_add(H_, c_t, ig, c_t);
+
+      // Compute output 
+      caffe_gpu_tanh(H_, c_t, tanh_c_t);
+      caffe_gpu_mul(H_, o_t, tanh_c_t, h_t);
+      
+      h_t += H_;
+      c_t += H_;
+      tanh_c_t += H_;
+      c_t_1 += H_;
+      i_t += 4*H_;
+      f_t += 4*H_;
+      o_t += 4*H_;
+      g_t += 4*H_;
+      pre_i_t += 4*H_;
+      pre_g_t += 4*H_;
+      mask_t += H_;
     }
-    caffe_gpu_mul(N_*H_, f_t, c_t_1, c_t);
-    caffe_gpu_mul(N_*H_, i_t, g_t, ig);
-    caffe_gpu_add(N_*H_, c_t, ig, c_t);
-
-    // Compute output 
-    caffe_gpu_tanh(N_*H_, c_t, tanh_c_t);
-    caffe_gpu_mul(N_*H_, o_t, tanh_c_t, h_t);
   }
-
   // Preserve cell state and output value for truncated BPTT
   caffe_copy(N_*H_, cell_data + cell_.offset(T_-1), c_T_.mutable_gpu_data());
   caffe_copy(N_*H_, top_data + top_.offset(T_-1), h_T_.mutable_gpu_data());
@@ -126,62 +138,80 @@ void LstmLayer<Dtype>::Backward_gpu(const vector<Blob<Dtype>*>& top,
     Dtype* dc_t = cell_diff + cell_.offset(t);
     const Dtype* tanh_c_t = tanh_cell_data + tanh_cell_.offset(t); 
     const Dtype* i_t = gate_data + gate_.offset(t);
-    const Dtype* f_t = gate_data + gate_.offset(t, 1); 
-    const Dtype* o_t = gate_data + gate_.offset(t, 2); 
-    const Dtype* g_t = gate_data + gate_.offset(t, 3); 
+    const Dtype* f_t = gate_data + gate_.offset(t, 0, 1); 
+    const Dtype* o_t = gate_data + gate_.offset(t, 0, 2); 
+    const Dtype* g_t = gate_data + gate_.offset(t, 0, 3); 
     Dtype* di_t = gate_diff + gate_.offset(t);
-    Dtype* df_t = gate_diff + gate_.offset(t, 1); 
-    Dtype* do_t = gate_diff + gate_.offset(t, 2); 
-    Dtype* dg_t = gate_diff + gate_.offset(t, 3); 
+    Dtype* df_t = gate_diff + gate_.offset(t, 0, 1); 
+    Dtype* do_t = gate_diff + gate_.offset(t, 0, 2); 
+    Dtype* dg_t = gate_diff + gate_.offset(t, 0, 3); 
     Dtype* pre_di_t = pre_gate_diff + pre_gate_.offset(t);
-    Dtype* pre_dg_t = pre_gate_diff + pre_gate_.offset(t, 3);
+    Dtype* pre_dg_t = pre_gate_diff + pre_gate_.offset(t, 0, 3);
     Dtype* fdc = fdc_.mutable_gpu_data();
     const Dtype* mask_t = mask + clip_mask_.offset(t);
 
-    // Output gate : tanh(c(t)) * h_diff(t)
-    caffe_gpu_mul(N_*H_, tanh_c_t, dh_t, do_t);
+    for (int n = 0; n < N_; ++n) {
+      // Output gate : tanh(c(t)) * h_diff(t)
+      caffe_gpu_mul(H_, tanh_c_t, dh_t, do_t);
 
-    // Cell state : o(t) * tanh'(c(t)) * h_diff(t) + f(t+1) * c_diff(t+1)
-    caffe_gpu_mul(N_*H_, o_t, dh_t, dc_t);
-    caffe_gpu_tanh_diff(N_*H_, tanh_c_t, dc_t, dc_t);
-    if (t < T_-1) {
-      caffe_gpu_mul(N_*H_, f_t + gate_.offset(1), dc_t + cell_.offset(1), fdc);
-      if (clip) {
-        caffe_gpu_mul(N_*H_, mask_t + clip_mask_.offset(1), fdc, fdc);
+      // Cell state : o(t) * tanh'(c(t)) * h_diff(t) + f(t+1) * c_diff(t+1)
+      caffe_gpu_mul(H_, o_t, dh_t, dc_t);
+      caffe_gpu_tanh_diff(H_, tanh_c_t, dc_t, dc_t);
+      if (t < T_-1) {
+        caffe_gpu_mul(H_, f_t + gate_.offset(1), dc_t + cell_.offset(1), fdc);
+        caffe_gpu_add(H_, fdc, dc_t, dc_t);
       }
-      caffe_gpu_add(N_*H_, fdc, dc_t, dc_t);
-    }
 
-    // Forget gate : c(t-1) * c_diff(t)
-    const Dtype* c_t_1 = t > 0 ? (c_t - cell_.offset(1)) : c_0_.gpu_data();
-    if (clip) {
-      caffe_gpu_mul(N_*H_, c_t_1, mask_t, clipped_.mutable_gpu_data());
-      c_t_1 = clipped_.gpu_data();
-    }
-    caffe_gpu_mul(N_*H_, c_t_1, dc_t, df_t);
+      // Forget gate : c(t-1) * c_diff(t)
+      const Dtype* c_t_1 = t > 0 ? (c_t - cell_.offset(1)) : c_0_.gpu_data();
+      if (clip) {
+        caffe_gpu_mul(H_, c_t_1, mask_t, clipped_.mutable_gpu_data());
+        c_t_1 = clipped_.gpu_data();
+      }
+      caffe_gpu_mul(H_, c_t_1, dc_t, df_t);
 
-    // Input gate : g(t) * c_diff(t)
-    caffe_gpu_mul(N_*H_, g_t, dc_t, di_t);
-    // Input modulation gate : i(t) * c_diff(t)
-    caffe_gpu_mul(N_*H_, i_t, dc_t, dg_t);
+      // Input gate : g(t) * c_diff(t)
+      caffe_gpu_mul(H_, g_t, dc_t, di_t);
+      // Input modulation gate : i(t) * c_diff(t)
+      caffe_gpu_mul(H_, i_t, dc_t, dg_t);
 
-    // Compute derivate before nonlinearity
-    caffe_gpu_sigmoid_diff(3*N_*H_, i_t, di_t, pre_di_t);
-    caffe_gpu_tanh_diff(N_*H_, g_t, dg_t, pre_dg_t);
+      // Compute derivate before nonlinearity
+      caffe_gpu_sigmoid_diff(3*H_, i_t, di_t, pre_di_t);
+      caffe_gpu_tanh_diff(H_, g_t, dg_t, pre_dg_t);
+      
+      // Clip deriviates before nonlinearity
+      if (clipping_threshold_ > 0.0f) {
+        caffe_gpu_bound(4*H_, pre_di_t, -clipping_threshold_, 
+            clipping_threshold_, pre_di_t);
+      }
 
-    // Clip deriviates before nonlinearity
-    if (clipping_threshold_ > 0.0f) {
-      caffe_gpu_bound(4*N_*H_, pre_di_t, -clipping_threshold_, 
-          clipping_threshold_, pre_di_t);
+      dh_t += H_;
+      c_t += H_;
+      dc_t += H_;
+      tanh_c_t += H_;
+      mask_t += H_;
+      di_t += 4*H_;
+      df_t += 4*H_;
+      do_t += 4*H_;
+      dg_t += 4*H_;
+      i_t += 4*H_;
+      f_t += 4*H_;
+      o_t += 4*H_;
+      g_t += 4*H_;
+      pre_di_t += 4*H_;
+      pre_dg_t += 4*H_;
     }
     
     if (t > 0) {
-      Dtype* dh_t_1 = dh_t - top_.offset(1);
+      Dtype* dh_t_1 = top_diff + top_.offset(t-1);
       // Backprop output errors to the previous time step
       caffe_gpu_gemm(CblasNoTrans, CblasNoTrans, N_, H_, 4*H_,
-        (Dtype)1., pre_di_t, weight_h, (Dtype)0., clipped_.mutable_gpu_data());
+          (Dtype)1., pre_gate_diff + pre_gate_.offset(t), 
+          weight_h, (Dtype)0., clipped_.mutable_gpu_data());
       if (clip) {
-        caffe_gpu_mul(N_*H_, clipped_.gpu_data(), mask_t, clipped_.mutable_gpu_data());
+        caffe_gpu_mul(N_*H_, clipped_.gpu_data(),
+          mask + clip_mask_.offset(t), 
+          clipped_.mutable_gpu_data());
       }
       caffe_gpu_add(N_*H_, dh_t_1, clipped_.gpu_data(), dh_t_1);
     }
@@ -215,6 +245,7 @@ void LstmLayer<Dtype>::Backward_gpu(const vector<Blob<Dtype>*>& top,
     caffe_gpu_gemm(CblasNoTrans, CblasNoTrans, T_*N_, I_, 4*H_, (Dtype)1.,
         pre_gate_diff, weight_i, (Dtype)0., bottom[0]->mutable_gpu_diff());
   }
+
 }
 
 INSTANTIATE_LAYER_GPU_FUNCS(LstmLayer);
