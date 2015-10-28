334c1a49d6c55d995d6f7e8ac01c11fdf0d55ba6
junhyuk <junhyuk@umich.edu>
2015-10-27 23:09:39 -0400

    Support GPU mode and change data layers to input

--- examples/lstm_sequence/lstm_sequence.cpp
+++ examples/lstm_sequence/lstm_sequence.cpp
@@ -10,7 +10,7 @@ using std::abs;
 using boost::shared_ptr;
 using namespace caffe;
 
-double f_x(double t) {
+float f_x(float t) {
   return 0.5*sin(2*t) - 0.05*cos(17*t + 0.8) 
       + 0.05*sin(25*t+10) - 0.02*cos(45*t + 0.3);
 }
@@ -30,37 +30,6 @@ int main(int argc, char** argv)
   caffe::SolverParameter solver_param;
   caffe::ReadProtoFromTextFileOrDie(net_solver, &solver_param);
 
-  shared_ptr<Solver<double> > solver;
-  solver.reset(GetSolver<double>(solver_param));
-  shared_ptr<Net<double> > train_net(solver->net());
-  shared_ptr<Net<double> > test_net(new Net<double> (solver_param.net(), TEST));
-  CHECK(train_net->has_layer("data"));
-  CHECK(train_net->has_layer("clip"));
-  CHECK(test_net->has_layer("data"));
-  CHECK(test_net->has_layer("clip"));
-  MemoryDataLayer<double>* train_data_layer = 
-    static_cast<MemoryDataLayer<double>*>(train_net->layer_by_name("data").get());
-  MemoryDataLayer<double>* train_clip_layer = 
-    static_cast<MemoryDataLayer<double>*>(train_net->layer_by_name("clip").get()); 
-  MemoryDataLayer<double>* test_data_layer = 
-    static_cast<MemoryDataLayer<double>*>(test_net->layer_by_name("data").get());
-  MemoryDataLayer<double>* test_clip_layer = 
-    static_cast<MemoryDataLayer<double>*>(test_net->layer_by_name("clip").get());
-  const LayerParameter& layer_param = train_data_layer->layer_param();
-  int seq_length = layer_param.memory_data_param().batch_size();
-  CHECK_EQ(TotalLength % seq_length, 0);
-
-  // Initialize bias for the forget gate to 5 as described in the clockwork RNN paper
-  const vector<shared_ptr<Layer<double> > >& layers = train_net->layers();
-  for (int i = 0; i < layers.size(); ++i) {
-    if (strcmp(layers[i]->type(), "Lstm") != 0) {
-      continue;
-    }
-    const int h = layers[i]->layer_param().lstm_param().num_output();
-    shared_ptr<Blob<double> > bias = layers[i]->blobs()[2];
-    caffe_set(h, 5.0, bias->mutable_cpu_data() + h);
-  }
-
   // Set device id and mode
   if (solver_param.solver_mode() == caffe::SolverParameter_SolverMode_GPU) {
     LOG(INFO) << "Use GPU with device ID " << solver_param.device_id();
@@ -71,18 +40,44 @@ int main(int argc, char** argv)
     Caffe::set_mode(Caffe::CPU);
   }
 
+  shared_ptr<Solver<float> > solver;
+  solver.reset(GetSolver<float>(solver_param));
+  shared_ptr<Net<float> > train_net(solver->net());
+  shared_ptr<Net<float> > test_net(new Net<float> (solver_param.net(), TEST));
+  CHECK(train_net->has_blob("data"));
+  CHECK(train_net->has_blob("clip"));
+  CHECK(train_net->has_blob("label"));
+  CHECK(test_net->has_blob("data"));
+  CHECK(test_net->has_blob("clip"));
+  shared_ptr<Blob<float> > train_data_blob = train_net->blob_by_name("data");
+  shared_ptr<Blob<float> > train_label_blob = train_net->blob_by_name("label");
+  shared_ptr<Blob<float> > train_clip_blob = train_net->blob_by_name("clip");
+  shared_ptr<Blob<float> > test_data_blob = test_net->blob_by_name("data");
+  shared_ptr<Blob<float> > test_clip_blob = test_net->blob_by_name("clip");
+  
+  const int seq_length = train_data_blob->shape(0);
+  CHECK_EQ(TotalLength % seq_length, 0);
+
+  // Initialize bias for the forget gate to 5 as described in the clockwork RNN paper
+  const vector<shared_ptr<Layer<float> > >& layers = train_net->layers();
+  for (int i = 0; i < layers.size(); ++i) {
+    if (strcmp(layers[i]->type(), "Lstm") != 0) {
+      continue;
+    }
+    const int h = layers[i]->layer_param().lstm_param().num_output();
+    shared_ptr<Blob<float> > bias = layers[i]->blobs()[2];
+    caffe_set(h, 5.0f, bias->mutable_cpu_data() + h);
+  }
+
   vector<int> sequence_shape(1, TotalLength);
   vector<int> data_shape(1, seq_length);
-  Blob<double> sequence(sequence_shape);
-  Blob<double> data(data_shape);
-  Blob<double> clip(data_shape);
-  caffe_set(seq_length, 1.0, clip.mutable_cpu_data());
+  Blob<float> sequence(sequence_shape);
 
   // Construct data 
-  double mean = 0;
-  double max_abs = 0;
+  float mean = 0;
+  float max_abs = 0;
   for (int i = 0; i < TotalLength; ++i) {
-    double val = f_x(i * 0.01);
+    float val = f_x(i * 0.01f);
     max_abs = max(max_abs, abs(val));
   }
   for (int i = 0; i < TotalLength; ++i) {
@@ -90,19 +85,25 @@ int main(int argc, char** argv)
   }
   mean /= TotalLength;
   for (int i = 0; i < TotalLength; ++i) {
-    sequence.mutable_cpu_data()[i] = f_x(i*0.01) / max_abs - mean;
+    sequence.mutable_cpu_data()[i] = f_x(i*0.01f) / max_abs - mean;
   }
 
   // Training
+  caffe_set(seq_length, 1.0f, train_clip_blob->mutable_cpu_data());
   int iter = 0;
-  double dummy;
   while(iter < solver_param.max_iter()) {
     int seq_idx = iter % (TotalLength / seq_length);
-    clip.mutable_cpu_data()[0] = seq_idx == 0 ? 0.0 : 1.0;
-    train_data_layer->Reset(data.mutable_cpu_data(), 
-      sequence.mutable_cpu_data() + sequence.offset(seq_idx * seq_length), 
-      seq_length);
-    train_clip_layer->Reset(clip.mutable_cpu_data(), &dummy, seq_length);
+    train_clip_blob->mutable_cpu_data()[0] = seq_idx > 0;
+    if (Caffe::mode() == Caffe::CPU) {
+      caffe_copy(seq_length, sequence.mutable_cpu_data() 
+          + sequence.offset(seq_idx * seq_length),
+          train_label_blob->mutable_cpu_data());
+    }
+    else {
+      caffe_copy(seq_length, sequence.mutable_gpu_data() 
+          + sequence.offset(seq_idx * seq_length),
+          train_label_blob->mutable_gpu_data());
+    }
     solver->Step(1);
     iter++;
   }
@@ -111,12 +112,14 @@ int main(int argc, char** argv)
   std::ofstream log_file;
   log_file.open(result_path, std::fstream::out);
   test_net->ShareTrainedLayersWith(train_net.get());
-  vector<Blob<double>* > bottom;
+  vector<Blob<float>* > bottom;
+  vector<int> shape(2, 1);
+  test_data_blob->Reshape(shape);
+  test_clip_blob->Reshape(shape);
+  test_net->Reshape();
   for (int i = 0; i < TotalLength; ++i) { 
-    double clip = i == 0 ? 0.0 : 1.0;
-    test_data_layer->Reset(data.mutable_cpu_data(), &dummy, 1);
-    test_clip_layer->Reset(&clip, &dummy, 1);
-    const vector<Blob<double>* >& pred = test_net->Forward(bottom);
+    test_clip_blob->mutable_cpu_data()[0] = i > 0;
+    const vector<Blob<float>* >& pred = test_net->ForwardPrefilled();
     CHECK_EQ(pred.size(), 1);
     CHECK_EQ(pred[0]->count(), 1);
     log_file << sequence.cpu_data()[i] << " " << *pred[0]->cpu_data() << std::endl;
